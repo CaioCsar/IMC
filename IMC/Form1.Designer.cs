2ef8c76e6a66e38ddaa8ca13c54b691393190885
Caio César <caiocsarbezerra95@gmail.com>
2022-03-29 22:40:17 -0300

    app

---
+++ IMC/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace IMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.result = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.Label();
+            this.sexo = new System.Windows.Forms.Label();
+            this.peso = new System.Windows.Forms.Label();
+            this.altura = new System.Windows.Forms.Label();
+            this.txt_nome = new System.Windows.Forms.TextBox();
+            this.txt_sexo = new System.Windows.Forms.TextBox();
+            this.txt_altura = new System.Windows.Forms.TextBox();
+            this.txt_peso = new System.Windows.Forms.TextBox();
+            this.result_box = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.limpar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(213, 349);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(75, 23);
+            this.result.TabIndex = 1;
+            this.result.Text = "Resultado";
+            this.result.UseVisualStyleBackColor = true;
+            this.result.Click += new System.EventHandler(this.result_Click);
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Location = new System.Drawing.Point(21, 27);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(38, 13);
+            this.name.TabIndex = 1;
+            this.name.Text = "Nome:";
+            // 
+            // sexo
+            // 
+            this.sexo.AutoSize = true;
+            this.sexo.Location = new System.Drawing.Point(21, 53);
+            this.sexo.Name = "sexo";
+            this.sexo.Size = new System.Drawing.Size(34, 13);
+            this.sexo.TabIndex = 2;
+            this.sexo.Text = "Sexo:";
+            // 
+            // peso
+            // 
+            this.peso.AutoSize = true;
+            this.peso.Location = new System.Drawing.Point(21, 79);
+            this.peso.Name = "peso";
+            this.peso.Size = new System.Drawing.Size(34, 13);
+            this.peso.TabIndex = 3;
+            this.peso.Text = "Peso:";
+            // 
+            // altura
+            // 
+            this.altura.AutoSize = true;
+            this.altura.Location = new System.Drawing.Point(21, 105);
+            this.altura.Name = "altura";
+            this.altura.Size = new System.Drawing.Size(37, 13);
+            this.altura.TabIndex = 4;
+            this.altura.Text = "Altura:";
+            // 
+            // txt_nome
+            // 
+            this.txt_nome.Location = new System.Drawing.Point(66, 24);
+            this.txt_nome.Name = "txt_nome";
+            this.txt_nome.Size = new System.Drawing.Size(100, 20);
+            this.txt_nome.TabIndex = 5;
+            // 
+            // txt_sexo
+            // 
+            this.txt_sexo.Location = new System.Drawing.Point(66, 50);
+            this.txt_sexo.Name = "txt_sexo";
+            this.txt_sexo.Size = new System.Drawing.Size(100, 20);
+            this.txt_sexo.TabIndex = 6;
+            this.txt_sexo.TextChanged += new System.EventHandler(this.txt_sexo_TextChanged);
+            // 
+            // txt_altura
+            // 
+            this.txt_altura.Location = new System.Drawing.Point(66, 102);
+            this.txt_altura.Name = "txt_altura";
+            this.txt_altura.Size = new System.Drawing.Size(100, 20);
+            this.txt_altura.TabIndex = 7;
+            this.txt_altura.TextChanged += new System.EventHandler(this.txt_altura_TextChanged);
+            // 
+            // txt_peso
+            // 
+            this.txt_peso.Location = new System.Drawing.Point(66, 76);
+            this.txt_peso.Name = "txt_peso";
+            this.txt_peso.Size = new System.Drawing.Size(100, 20);
+            this.txt_peso.TabIndex = 8;
+            this.txt_peso.TextChanged += new System.EventHandler(this.txt_peso_TextChanged);
+            // 
+            // result_box
+            // 
+            this.result_box.AutoSize = true;
+            this.result_box.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result_box.Location = new System.Drawing.Point(128, 177);
+            this.result_box.Name = "result_box";
+            this.result_box.Size = new System.Drawing.Size(213, 80);
+            this.result_box.TabIndex = 9;
+            this.result_box.Text = "                                                   \r\n                            " +
+    "       \r\n                                                   \r\n                  " +
+    "                ";
+            this.result_box.Click += new System.EventHandler(this.result_box_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(178, 177);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(152, 131);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // limpar
+            // 
+            this.limpar.Location = new System.Drawing.Point(213, 349);
+            this.limpar.Name = "limpar";
+            this.limpar.Size = new System.Drawing.Size(75, 23);
+            this.limpar.TabIndex = 0;
+            this.limpar.Text = "Limpar";
+            this.limpar.UseVisualStyleBackColor = true;
+            this.limpar.Click += new System.EventHandler(this.limpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 384);
+            this.Controls.Add(this.limpar);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.result_box);
+            this.Controls.Add(this.txt_peso);
+            this.Controls.Add(this.txt_altura);
+            this.Controls.Add(this.txt_sexo);
+            this.Controls.Add(this.txt_nome);
+            this.Controls.Add(this.altura);
+            this.Controls.Add(this.peso);
+            this.Controls.Add(this.sexo);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.result);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IMC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button result;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label sexo;
+        private System.Windows.Forms.Label peso;
+        private System.Windows.Forms.Label altura;
+        private System.Windows.Forms.TextBox txt_nome;
+        private System.Windows.Forms.TextBox txt_sexo;
+        private System.Windows.Forms.TextBox txt_altura;
+        private System.Windows.Forms.TextBox txt_peso;
+        private System.Windows.Forms.Label result_box;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button limpar;
+    }
+}
+
---
+++ IMC/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void result_box_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void result_Click(object sender, EventArgs e)
+        {
+            string nome = txt_nome.Text;
+            string sexo = txt_sexo.Text.ToLower();
+            double peso = double.Parse(txt_peso.Text);
+            double alt = double.Parse(txt_altura.Text);
+            double imc = peso / (alt * alt);
+            string status = "";
+
+
+            // Masculino
+            if(imc< 20.7 & (sexo=="m" || sexo == "masculino"))
+            {
+                status = "Abaixo do Peso";
+            }
+            if (imc > 20.7 && imc < 26.4 & (sexo == "m" || sexo == "masculino"))
+            {
+                status = "Peso Normal";
+            }
+            if (imc > 26.4 && imc < 27.8 & (sexo == "m" || sexo == "masculino"))
+            {
+                status = "Marginalmente acima do peso";
+            }
+            if (imc > 27.8 && imc < 31.1 & (sexo == "m" || sexo == "masculino"))
+            {
+                status = "Acima do peso";
+            }
+            if (imc > 31.1 & (sexo == "m" || sexo == "masculino"))
+            {
+                status = "Obeso";
+            }
+
+            //Feminino
+            if (imc < 19.1 & (sexo == "f" || sexo == "feminino"))
+            {
+                status = "Abaixo do Peso";
+            }
+            if (imc > 19.1 && imc < 25.8 & (sexo == "f" || sexo == "feminino"))
+            {
+                status = "Peso Normal";
+            }
+            if (imc > 25.8 && imc < 27.3 & (sexo == "f" || sexo == "feminino"))
+            {
+                status = "Marginalmente acima do peso";
+            }
+            if (imc > 27.3 && imc < 32.3 & (sexo == "f" || sexo == "feminino"))
+            {
+                status = "Acima do peso";
+            }
+            if (imc > 32.3 & (sexo == "f" || sexo == "feminino"))
+            {
+                status = "Obeso";
+            }
+
+            //Condicao normal
+            double imcIdeal = 0;
+            double pesoIdeal = 0;
+            double pesoId = 0;
+            string sts = "";
+
+            if (sexo == "m" || sexo == "masculino")
+            {
+                imcIdeal = (20.7 + 26.4) / 2;
+            }
+            if(sexo == "f" || sexo == "feminino")
+            {
+                imcIdeal = (19.1 + 25.8) / 2;
+            }
+
+            //-------
+
+            if (imc > imcIdeal & status != "Peso Normal")
+            {
+                pesoIdeal = imcIdeal * (alt * alt);
+                pesoId = (pesoIdeal - peso)*(-1);
+                sts = "Para ficar com o peso ideal, deve perder" 
+                    + " " + Math.Round(pesoId, 2) + "kg";
+            }
+            if (imc < imcIdeal & status != "Peso Normal")
+            {
+                pesoIdeal = imcIdeal * (alt * alt);
+                pesoId = pesoIdeal - peso;
+                sts = "Para ficar com o peso ideal, deve ganhar" 
+                    + " " + Math.Round(pesoId, 2) + "kg";
+            }
+
+            //----Resultado ----
+
+            result_box.Text = 
+                "Nome: " + nome +"\n"+
+                "Sexo: " + sexo + "\n" +
+                "Peso atual: " + peso + "\n" +
+                "Altura: " + alt + "\n" +
+                "IMC: " + Math.Round(imc, 2) + "\n" +
+                "Status: " + status + "\n" + 
+                 sts ;
+
+            // - -
+            pictureBox1.Visible = false;
+            result.Visible = false;
+            limpar.Visible = true;
+
+        }
+
+        private void txt_sexo_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_peso_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_altura_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void limpar_Click(object sender, EventArgs e)
+        {
+            limpar.Visible = false;
+
+            txt_nome.Clear();
+            txt_peso.Clear();
+            txt_altura.Clear();
+            txt_sexo.Clear();
+            result_box.Text = " ";
+
+            pictureBox1.Visible = true;
+            result.Visible = true;
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            result.Visible = true;
+            limpar.Visible = false;
+        }
+    }
+}
